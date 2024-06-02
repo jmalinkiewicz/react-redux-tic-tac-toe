671a3ab8ca7c8307e2798bf7bfa468c30abeda09
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2024-06-02 16:45:27 +0200

    create three-by-three grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import clickSound from "./assets/sounds/click.wav";
+import CircleIcon from "./components/icons/circle";
+import CrossIcon from "./components/icons/cross";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">("X");
+
+  function handleClick(index: number) {
+    if (board[index]) return;
+
+    new Audio(clickSound).play();
+    const newBoard = [...board];
+    newBoard[index] = currentPlayer;
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+    setBoard(newBoard);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen w-screen flex justify-center items-center">
+        <div className="grid grid-cols-3">
+          {board.map((cell, index) => (
+            <div className={`h-32 w-32 p-2 cell ${"cell" + "-" + index}`}>
+              <div
+                onClick={() => handleClick(index)}
+                className={`grid place-content-center w-full h-full transition-colors p-2 ${
+                  !cell ? "hover:bg-neutral-200" : ""
+                }`}
+              >
+                {cell === "X" && <CrossIcon />}
+                {cell === "O" && <CircleIcon />}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
---
+++ src/components/icons/circle.tsx
@@ -0,0 +1,12 @@
+export default function CircleIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      id="Outline"
+      viewBox="0 0 24 24"
+      className="w-full h-full fill-black/80"
+    >
+      <path d="M12,0A12,12,0,1,0,24,12,12,12,0,0,0,12,0Zm0,22A10,10,0,1,1,22,12,10,10,0,0,1,12,22Z" />
+    </svg>
+  );
+}
